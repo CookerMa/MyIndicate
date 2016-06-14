9f3ef17f19c4a096791638b963551a50b5bbd6d7
majun <mj0120@foxmail.com>
2016-06-14 15:40:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myindicate/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.administrator.myindicate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Window;
+
+public class MainActivity extends FragmentActivity
+{
+    private List<Fragment> mTabContents = new ArrayList<Fragment>();
+    private FragmentPagerAdapter mAdapter;
+    private ViewPager mViewPager;
+    private List<String> mDatas = Arrays.asList("a", "b", "c", "d",
+            "e", "f", "g", "h", "i");
+
+    private TabIndicatorDemo mIndicator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.vp_indicator);
+
+        initView();
+        initDatas();
+        mIndicator.setTabItemTitles(mDatas);
+        mViewPager.setAdapter(mAdapter);
+        mIndicator.setViewPager(mViewPager,0);
+
+    }
+
+    private void initDatas()
+    {
+
+        for (String data : mDatas)
+        {
+            TestSimpleFragment fragment = TestSimpleFragment.newInstance(data);
+            mTabContents.add(fragment);
+        }
+
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager())
+        {
+            @Override
+            public int getCount()
+            {
+                return mTabContents.size();
+            }
+
+            @Override
+            public Fragment getItem(int position)
+            {
+                return mTabContents.get(position);
+            }
+        };
+    }
+
+    private void initView()
+    {
+        mViewPager = (ViewPager) findViewById(R.id.id_vp);
+        mIndicator = (TabIndicatorDemo) findViewById(R.id.id_indicator);
+    }
+
+
+}
